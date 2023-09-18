bad262e7d5e63265f9f161e9326e28e2c59fe5b3
Joel Cofie <joel.t.cofie@gmail.com>
2023-09-18 21:27:07 +0000

    removed redundant scripts

--- supabase/migrations/20230911170020_create_personalities_policy.sql
+++
@@ -1,6 +0,0 @@
-alter table personalities enable row level security;
-
-CREATE POLICY "Enable read access for all users" ON "public"."personalities"
-AS PERMISSIVE FOR SELECT
-TO public
-USING (true);
