c05fc778dfc70b672b6ad31e11562415d03b22fa
Joel Cofie <joel.t.cofie@gmail.com>
2023-09-28 09:58:48 +0000

    update schema types to include auth schema

--- supabase/seed.sql
+++
@@ -1,9 +0,0 @@
-
-insert into public.personalities (title, slug)
-VALUES
-('Realistic', 'realistic'),
-('Artistic', 'artistic'),
-('Investigative', 'investigative'),
-('Social', 'social'),
-('Enterprising', 'enterprising'),
-('Conventional', 'conventional');
